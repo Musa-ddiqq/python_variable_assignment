2a568330f60455e87b76063ac4aec5d843f0b86d
Musaddiq Qureshi <musaddiqqureshii@gmail.com>
2025-05-09 23:16:00 +0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,48 @@
+#PART 1 = Declare variable 
+
+# Variable storing a student's name 
+student_name = "Musaddiq"  
+
+# Variable storing a student's ID 
+student_id = "ML-25-PYB-0052  "
+
+# Variable storing a age
+age = 21  
+
+# Print all variables
+print("Name:", student_name)
+print("ID:", student_id)
+print("Age:", age)
+
+#-------------
+
+#PART 2 = INVALID variables examples
+
+# 1.
+2name = "musa"  
+#  Error: Variable names cannot begin with a digit. 
+#  correct: Use 'name2' or 'second_name' instead.
+
+# 2.
+my-name = "musa"  
+#  Error: Hyiphens are interpreted as subtraction operators.
+#  correct: Use 'my_name' or 'myName' (snake_case or camelCase).
+
+# 3. 
+user name = "musa"  
+#  Error: Spaces are not allowed in variable names.
+#  Corect: Use 'user_name' or 'username'.
+
+       #---------------
+
+#PART 3 = Invalide Variables example
+# variables following Python's snake_case 
+
+# Stores the full name of a student (string data type)
+student_name = "musa"  
+
+# Stores the age of the student in years (integer ) 
+student_age = 18  
+
+# Stores the aggregate marks scored by the student (float )
+total_marks = 95.8
